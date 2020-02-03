49e850023a605e66a1aa49fe09d2d14a21752cc9
Celio Caus Junior <celiocausjunior@gmail.com>
2020-02-02 21:52:12 -0300

    Update Exception

--- src/main/java/com/celiocausjr/course/services/UserService.java
+++ src/main/java/com/celiocausjr/course/services/UserService.java
@@ -3,10 +3,11 @@ package com.celiocausjr.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.support.DaoSupport;
 import org.springframework.stereotype.Service;
 
 import com.celiocausjr.course.entities.User;
@@ -45,15 +46,19 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 
 }
